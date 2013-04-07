ef86d063b2f8b3b5f708dbe06cadb8d6cf216418
Rocio Guzman <mroguzman@gmail.com>
2013-04-07 19:57:45 +0200

    Añadido proyecto Rails-Nominas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nominas::Application.config.secret_token = '75cab820608123518af4d165397df7536aa569d7928e914b0b1d259a9d5125152b3636a096cb7e9f8b9544c959489b900c1adbaf6dbd8e581bfb7328d4eb4b59'
