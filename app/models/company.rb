6cc1c03843df008e1eda1eae3a2c03d3e6e5fc50
Rocio Guzman <mroguzman@gmail.com>
2013-04-14 19:32:56 +0200

    añadidas validaciones al modelo company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ActiveRecord::Base
   attr_accessible :cif, :name, :sec_social_number, :street
+  
+  validates :cif, presence: true, uniqueness: { case_sensitive: false }, length: { is: 9 }
+  validates :name, presence: true
+  validates :sec_social_number, presence: true, numericality: true, length: { is: 10 }
 end
