d7970dbdad8e337bbba471701e204c729e9f79db
Rocio Guzman <mroguzman@gmail.com>
2013-04-14 19:25:35 +0200

    modificacion del modelo eemployee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,12 +1,13 @@
 class Employee < ActiveRecord::Base
   attr_accessible :name, :nif, :sec_social_number, :company, 
-  	:contribution_group
+    :contribution_group
 
   belongs_to :company
   belongs_to :contribution_group
   
   validates :name, presence: true
   validates :nif, presence: true, uniqueness: { case_sensitive: false }
-  validates :sec_social_number, numericality: true, length: { is: 10 }
+  validates :sec_social_number, presence: true, numericality: true, length: { is: 10 }
   validates :company, presence: true
+  validates :contribution_group, presence: true
 end
