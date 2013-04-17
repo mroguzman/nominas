85ee589404c07e7320fe34cbd4481e90d6e92695
Rocio Guzman <mroguzman@gmail.com>
2013-04-17 19:45:48 +0200

    Relacion Company-Payroll através de Employee

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ActiveRecord::Base
   attr_accessible :cif, :name, :sec_social_number, :street
 
   has_many :employees
+  has_many :payrolls, through: :employees
   
   validates :cif, presence: true, uniqueness: { case_sensitive: false }, length: { is: 9 }
   validates :name, presence: true
