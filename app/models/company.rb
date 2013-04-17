39f9c21244614f740959a21aa157f8524fb19d37
Rocio Guzman <mroguzman@gmail.com>
2013-04-17 18:49:17 +0200

    Añadido: Relacion de Company con Employee

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ActiveRecord::Base
   attr_accessible :cif, :name, :sec_social_number, :street
+
+  has_many :employees
   
   validates :cif, presence: true, uniqueness: { case_sensitive: false }, length: { is: 9 }
   validates :name, presence: true
