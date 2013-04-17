c5ff17722fdc2f1a44d7ea6e40c572a83d265221
Rocio Guzman <mroguzman@gmail.com>
2013-04-17 19:29:02 +0200

    Añadido:Validaciones Payroll

--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -1,5 +1,13 @@
 class Payroll < ActiveRecord::Base
-  attr_accessible :bonus, :end_date, :irpf, :no_bonuses, :overtime, :payment_in_kind, :salary, :salary_bonus, :social_sec_contribution, :start_date
+  attr_accessible :bonus, :end_date, :irpf, :no_bonuses, :overtime, :payment_in_kind, :salary, :salary_bonus, 
+  	:social_sec_contribution, :start_date
 
   belongs_to :employee
+
+  validates :end_date, presence: true
+  validates :irpf, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 } 
+  validates :salary, presence: true
+  validates :social_sec_contribution, presence: true
+  validates :start_date, presence: true
+
 end
