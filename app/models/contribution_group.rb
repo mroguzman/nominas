2a13c5d6c6cc1a30364517d583e77309492c8630
Rocio Guzman <mroguzman@gmail.com>
2013-04-20 18:06:34 +0200

    Relacion Contribution_group con Employee

--- app/models/contribution_group.rb
+++ app/models/contribution_group.rb
@@ -1,5 +1,7 @@
 class ContributionGroup < ActiveRecord::Base
   attr_accessible :number, :professional_category, :min_base_salary, :max_base_salary
+
+  has_many :employees
   
   validates :number, presence: true, uniqueness: true
   validates :professional_category, presence: true, uniqueness: { case_sensitive: false }
