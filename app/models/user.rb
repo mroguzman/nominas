ed164bd493b0916c18a5cce3ba4afd1a9b310ba7
Rocio Guzman <mroguzman@gmail.com>
2013-04-20 18:31:31 +0200

    Modificacion modelo User mas installar la gema bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_digest
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX } 
+
+  has_secure_password
 end
