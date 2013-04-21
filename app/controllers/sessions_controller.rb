44bcec1ccd6b44b47bd9e8c756f71e1bf02abfbd
Rocio Guzman <mroguzman@gmail.com>
2013-04-21 19:25:37 +0200

    Autentificacion del usuario

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class SessionsController < ApplicationController
   skip_before_filter :authorize
 
@@ -5,6 +7,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to payrolls_url
+    else
+      redirect_to root_url, alert: "Usuario o password inválidos"
+    end
   end
 
   def destroy
