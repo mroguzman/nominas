03c434c599e473499b4881966e52c8e9c7fdfb4c
Rocio Guzman <mroguzman@gmail.com>
2013-04-23 13:07:10 +0200

    validacion del campo email del modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_one :company
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX } 
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
   has_secure_password
 end
