80799ea597cd13c863c4aa711017f19644025f31
José Francisco Calvo <josefranciscocalvo@gmail.com>
2013-05-12 19:57:12 +0200

    Añadida relacion de usuario con nóminas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
-  
+
   has_one :company
-  
+  has_many :payrolls, through: :company
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, length: { minimum: 6 }
-  
+
   has_secure_password
 
   def name
