8eaea6c3444ee2dccbaa8a12bfc306d2108740c4
Rocio Guzman <mroguzman@gmail.com>
2013-05-23 18:48:25 +0200

    Landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Nominas::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root to: "sessions#login"
+  root to: "sessions#landing"
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy, :login]
   resources :dashboard, only: [:index]
 
   resources :users, only: [:show, :new, :create]
