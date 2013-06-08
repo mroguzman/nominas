e1409333cf282c1d30c77779645d3aa1180d938c
José Francisco Calvo <josefranciscocalvo@gmail.com>
2013-06-08 19:00:57 +0200

    Añade helper number_to_euros

--- app/helpers/payrolls_helper.rb
+++ app/helpers/payrolls_helper.rb
@@ -1,5 +1,11 @@
+# encoding: utf-8
+
 module PayrollsHelper
   def years_for_payrolls(payrolls)
     payrolls.map(&:start_date).map(&:year).uniq.sort.reverse
+  end
+
+  def number_to_euros(number)
+    "#{number_with_precision(number, precision: 2)} €"
   end
 end
